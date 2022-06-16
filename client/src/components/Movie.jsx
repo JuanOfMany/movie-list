a4d1991665c5a9623cd2e4a29d4f4df8a4db02f8
Juan Pinol <JuannnCodes@gmail.com>
2022-06-15 23:55:49 -0400

    Implemented a watched button for each movie that toggles colors based on user input.

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const Movie = (props) => {
-  return (
-  <li>{props.movie.title}</li>
-  );
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+  this.state = {
+    watched : false,
+   }
+
+   this.handleWatchClick = this.handleWatchClick.bind(this)
+  }
+
+  handleWatchClick = function(event) {
+    if (this.state.watched) {
+      event.target.style.backgroundColor = 'green'
+      this.setState({watched : false})
+    } else {
+      event.target.style.backgroundColor = 'gray'
+      this.setState({watched :true})
+    }
+  }
+
+  render() {
+    return (
+    <li>
+      {this.props.movie.title}
+      <button onClick={this.handleWatchClick}>Watched</button>
+    </li>
+    );
+  }
 }
 
 export default Movie
